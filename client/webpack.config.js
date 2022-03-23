d0ecefed2d81fccdb88befdf923066e603fe9a92
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-23 14:23:32 -0700

    bug fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 
@@ -9,40 +9,36 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: ""
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
 
-   // Webpack plugin that generates our html file and injects our bundles.
-  new HtmlWebpackPlugin({
-    template: "./index.html",
-    title: "JAST",
-  }),
-  
-
-  // Injects our custom service worker
-  new InjectManifest({
-    swSrc: "./src-sw.js",
-    swDest: "src-sw.js",
-  }),
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
 
       // Creates a manifest.json file.
-  new WebpackPwaManifest({
+      new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "JAST",
-        short_name: "JAST",
+        name: "Just Another Text Editor",
+        short_name: "JATE",
         description: "Texts whenever you need them!",
         background_color: "#272822",
-
         publicPath: "/",
         icons: [
           {
@@ -52,7 +48,6 @@ module.exports = () => {
           },
         ],
       }),
-    
     ],
 
     module: {
@@ -65,7 +60,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-    //  Babel loader
+          //  Babel loader
           use: {
             loader: "babel-loader",
             options: {
