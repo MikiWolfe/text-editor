8f54d5e10e7fb758646c237e14bec40bdcf70956
Mikayla Bruce <mkla.w.b@gmail.com>
2022-03-23 12:22:34 -0700

    Pushing to Heroku again

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -24,7 +24,7 @@ export const putDb = async (content) => {
   console.log("Save the data!", result);
 };
 // logic for a method that gets all the content from the database
-export const getAllDb = async () => {
+export const getDb = async () => {
   console.log("GET all the Data!");
   console.error("getDb not implemented");
   const jateDb = await openDB("jate", 1);
